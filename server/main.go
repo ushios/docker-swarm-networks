53b9a9cb49cafbe89e2ba517ac37a8eb3195f248
ushio <ushio.s@gmail.com>
2017-11-16 14:28:03 +0900

    launch server.

--- server/main.go
+++ server/main.go
@@ -5,18 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 var (
-	name = flag.String("name", "", "server name")
+	name string
 	port = flag.Int("port", 8080, "server port")
 )
 
 func main() {
 	flag.Parse()
 
+	name = os.Getenv("HOSTNAME")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, *name)
+		log.Println(name)
+		fmt.Fprintf(w, name)
 	})
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", *port), nil))
